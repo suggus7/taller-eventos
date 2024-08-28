a5faa76778186a6399efe0f661c8dfd32188aa36
suggus7 <ad.delgado@hotmail.com>
2024-08-28 16:33:27 -0300

    solucion para evitar la propagacion del click

--- eventos.js
+++ eventos.js
@@ -2,3 +2,9 @@ const divClick = document.getElementById("divClick");
 divClick.addEventListener('click', function() {
     alert("Hola! Soy el div");
 });
+
+const button = document.querySelector("#divClick button");
+button.addEventListener('click', function(event) {
+    event.stopPropagation(); 
+    alert('Hola! Soy el botón');
+});
